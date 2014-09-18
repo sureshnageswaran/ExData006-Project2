64894fe1e1aeddd0fe905621a79cdb893f7653e0
Suresh Nageswaran <sureshnageswaran@yahoo.com>
2014-09-18 12:42:07 -0400

    Final-2
    Changes to final code.

--- plot5.r
+++ plot5.r
@@ -1,5 +1,9 @@
+# Please visit https://github.com/sureshnageswaran/ExData006-Project2
+# View this readme file before using the code
+# https://github.com/sureshnageswaran/ExData006-Project2/blob/master/README.md
 
-source("checkAndDownload.r") # Contains code to download the files in case absent
+# The source file mentioned below is no longer needed, since the function has been appended to the end of this code file.
+#source("checkAndDownload.r") # Contains code to download the files in case absent
 
 library(plyr) # For join(), which is faster than merge() for the size of the dataframe
 library(ggplot2) # For qplot()
@@ -98,3 +102,50 @@ plot5 <- function(sPath="C:/projects/rwork/exdata006/project2/ExData006-Project2
   return (TRUE)
   
  }
+ 
+ # Function : checkAndDownload
+ # Author   : Suresh Nageswaran
+ # Input    : Path to the dataset, 
+ # Output   : Boolean
+ #            True if the given path was valid; False otherwise
+ # Dependencies : None
+ # How to invoke: On command line, simply type > checkAndDownload("<Path>")
+ # Purpose  : This function checks if the input files required are present.
+ #            If absent, they are downloaded into the current folder.
+ 
+ checkAndDownload <- function(sPath="")
+ {
+  # List of required files
+   files <- c(
+             "summarySCC_PM25.rds",           # NEI
+             "Source_Classification_Code.rds" # SCC
+            )     
+            
+  if( ! file.exists(sPath) )
+  {
+     # Either no path was given or path was wrong
+     # In this case, we set the current directory as the path
+     sPath = getwd()
+     setwd(sPath)
+     retVal <- FALSE
+   }
+   else
+   {
+     retVal <- TRUE
+   }
+   
+  # Check if the files are in the working directory     
+   if( !all(file.exists(files)) )
+   {    
+     # download the files
+     temp <- tempfile()
+     fileURL <-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+     download.file(fileURL,temp)
+     unzip(temp,files=files)
+     file.remove(temp)    
+   }
+ 
+   
+   return (retVal)
+ 
+}
